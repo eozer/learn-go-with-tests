da9a94d1fb70b5e3d177967801acbf8f2da40112
Ege Can Ozer <egecan.ozer@gmail.com>
2020-10-27 00:19:34 +0200

    smi: Add final impl. for 5th exercise.
    Recap:
    - new syntax for struct, interface, lambda struct def.
    - func (f foo) Bar(foobar int) int is a method signature.
    - interface resolution is implicit, no one says X implements Y.
    - Use %#v to display struct along with its values.

--- smi/shapes.go
+++ smi/shapes.go
@@ -1,5 +1,11 @@
 package shapes
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,10 +15,27 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(r Rectangle) float64 {
 	return 2.0 * (r.Width + r.Height)
 }
 
 func Area(r Rectangle) float64 {
 	return r.Width * r.Height
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
 }
--- smi/shapes_test.go
+++ smi/shapes_test.go
@@ -14,21 +14,36 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+
+	checkArea := func(t *testing.T, s Shape, want float64) {
+		got := s.Area()
+		if got != want {
+			t.Errorf("%#v got %g, want %g", s, got, want)
+		}
+	}
+
 	t.Run("computes Area of a rectangle", func(t *testing.T) {
 		r := Rectangle{10.0, 10.0}
-		got := Area(r)
-		want := 100.0
-		if got != want {
-			t.Errorf("got %.2f, want %.2f", got, want)
-		}
+		checkArea(t, r, 100.0)
 	})
 
 	t.Run("computes Area of a circle", func(t *testing.T) {
 		c := Circle{10}
-		got := Area(c)
-		want := 314.1592653589793
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
+		checkArea(t, c, 314.1592653589793)
+	})
+
+	t.Run("table driven tests: rectangle, circle, triangle...", func(t *testing.T) {
+		tests := []struct {
+			shape   Shape
+			hasArea float64
+		}{
+			{shape: Rectangle{10.0, 10.0}, hasArea: 100.0},
+			{shape: Circle{10.0}, hasArea: 314.1592653589793},
+			{shape: Triangle{12, 6}, hasArea: 36.0},
+		}
+		for _, test := range tests {
+			checkArea(t, test.shape, test.hasArea)
 		}
 	})
+
 }
