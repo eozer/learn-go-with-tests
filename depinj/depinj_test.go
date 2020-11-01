3894ee40f00d45432a9a7d69ed85a09120269a99
Ege Can Ozer <egecan.ozer@gmail.com>
2020-11-01 15:32:10 +0200

    depinj: Add initial dependency injection exercise.

---
+++ depinj/depinj_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+
+	t.Run("initial test", func(t *testing.T) {
+		buffer := bytes.Buffer{} // bytes i
+		Greet(&buffer, "Foobar")
+		got := buffer.String()
+		want := "Hello Foobar"
+		assertStrings(t, got, want)
+	})
+
+}
+
+func assertStrings(t *testing.T, got string, want string) {
+	t.Helper()
+	if want != got {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
