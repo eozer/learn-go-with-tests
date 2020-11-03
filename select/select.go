d798a7e18f1036593feb1c6f60bca95d0622eb0e
Ege Can Ozer <egecan.ozer@gmail.com>
2020-11-03 13:11:16 +0000

    select: Finalized the exercise.
    Recap:
    - always make chan
    - select is used to handle multiple channels
    - struct {} doesnt allocate
    - time.After returns chan, can be used to handle timeout for chan
    - httptest is useful to intercept http requests.

--- select/select.go
+++ select/select.go
@@ -1,27 +1,32 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"time"
 )
 
-// measureHTTPResponse returns the duration took to return a response from URL.
-func measureHTTPResponse(URL string) time.Duration {
-	start := time.Now()
-	http.Get(URL)
-	duration := time.Since(start) // shorthand for: time.Now().Sub(start)
-	return duration
+// Racer is a function
+func Racer(URL1, URL2 string) (string, error) {
+	select {
+	case <-ping(URL1):
+		return URL1, nil
+	case <-ping(URL2):
+		return URL2, nil
+	case <-time.After(10 * time.Second):
+		return "", errors.New("Timeout error")
+	}
 }
 
-// Racer is a function
-func Racer(URL1, URL2 string) (fastestURL string) {
-	duration1 := measureHTTPResponse(URL1)
-	duration2 := measureHTTPResponse(URL2)
-	// If duration1 is smaller URL1 is faster.
-	if duration1 < duration2 {
-		return URL1
-	}
-	return URL2
+// NOTE: struct {} does not allocate anything in memory when make(chan struct{}) vs
+// make(chan bool)
+func ping(URL string) chan struct{} {
+	ch := make(chan struct{}) // NOTE: always use make vs -> var ch struct {}, because ch is now nil
+	go func() {
+		http.Get(URL)
+		close(ch)
+	}()
+	return ch
 }
 
 func main() {
--- select/select_test.go
+++ select/select_test.go
@@ -18,7 +18,7 @@ func TestRacer(t *testing.T) {
 	slowURL := "http://www.facebook.com"
 	fastURL := "http://www.quii.co.uk"
 	t.Run("old way", func(t *testing.T) {
-		got := Racer(slowURL, fastURL)
+		got, _ := Racer(slowURL, fastURL)
 		want := fastURL
 		if want != got {
 			t.Errorf("want %q, got %q", want, got)
@@ -30,11 +30,24 @@ func TestRacer(t *testing.T) {
 		fastServer := makeDelayedHTTPServer(100 * time.Millisecond)
 		defer slowServer.Close()
 		defer fastServer.Close()
-
-		got := Racer(slowURL, fastURL)
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+		got, _ := Racer(slowURL, fastURL)
 		want := fastURL
 		if want != got {
 			t.Errorf("want %q, got %q", want, got)
+		}
+	})
+
+	t.Run("returns an error if response takes more than 10 seconds", func(t *testing.T) {
+		slowServer := makeDelayedHTTPServer(11 * time.Second)
+		defer slowServer.Close()
+		fastServer := makeDelayedHTTPServer(12 * time.Second)
+		defer fastServer.Close()
+
+		_, err := Racer(slowServer.URL, fastServer.URL)
+		if err == nil {
+			t.Error("expected to receive an error")
 		}
 	})
 }
