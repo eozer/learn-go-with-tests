d93b54d3772aa4a3e7cbfab46668a649053458f8
Ege Can Ozer <egecan.ozer@gmail.com>
2020-12-06 12:47:53 +0000

    romannums: add naive impl that converts up to 40.

--- romannums/romannums.go
+++ romannums/romannums.go
@@ -1,8 +1,45 @@
 package romannums
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 2 {
-		return "II"
+	var result strings.Builder
+	for arabic > 0 {
+		switch {
+		case arabic > 9: // 10, ...
+			result.WriteString("X")
+			arabic -= 10
+		case arabic > 8: // 9, 10, ...
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4: // 5, 6, ...
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
 	}
-	return "I"
+	return result.String()
+}
+
+// TODO: exercise claims that string builder is more efficient than
+// plain string concat operation.
+func ConvertToRomanNoStringBuilder(arabic int) string {
+	result := ""
+	for i := arabic; i > 0; i-- {
+		if arabic == 5 {
+			result += "V"
+			break
+		}
+		if arabic == 4 {
+			result += "IV"
+			break
+		}
+		result += "I"
+	}
+	return result
 }
--- romannums/romannums_test.go
+++ romannums/romannums_test.go
@@ -2,22 +2,45 @@ package romannums
 
 import "testing"
 
+var testcases = []struct {
+	Description string
+	Arabic      int
+	Want        string
+}{
+	{"1 gets converted to I", 1, "I"},
+	{"2 gets converted to II", 2, "II"},
+	{"3 gets converted to III", 3, "III"},
+	{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+	{"5 gets converted to V", 5, "V"},
+	{"8 gets converted to VIII", 8, "VIII"},
+	{"9 gets converted to IX", 9, "IX"},
+	{"10 gets converted to X", 10, "X"},
+	{"14 gets converted to XIV", 14, "XIV"},
+	{"18 gets converted to XVIII", 18, "XVIII"},
+	{"20 gets converted to XX", 20, "XX"},
+	{"39 gets converted to XXXIX", 39, "XXXIX"},
+}
+
+func BenchmarkConvertToRoman(t *testing.B) {
+	for _, test := range testcases {
+		ConvertToRoman(test.Arabic)
+	}
+}
+
+func BenchmarkConvertToRomanNoStringBuilder(t *testing.B) {
+	for _, test := range testcases {
+		ConvertToRomanNoStringBuilder(test.Arabic)
+	}
+}
+
 func TestConvertToRoman(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	for _, test := range testcases {
+		t.Run(test.Description, func(t *testing.T) {
+			t.Helper()
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
 }
